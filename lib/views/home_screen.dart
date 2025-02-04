2115063adbd8a0cef67cc79e30b3f79f33268165
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-02-04 23:54:26 +0700

    adding some Button Navigation Bar Items

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -4,7 +4,52 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80), // Sesuaikan tinggi AppBar
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue, // Ganti warna sesuai selera
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 4,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.menu, color: Colors.white),
+                  onPressed: () {
+                    // Aksi ketika menu ditekan
+                  },
+                ),
+                Text(
+                  'Home',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.notifications, color: Colors.white),
+                  onPressed: () {
+                    // Aksi ketika notifikasi ditekan
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Center(child: Text('Ini halaman Home')),
     );
   }

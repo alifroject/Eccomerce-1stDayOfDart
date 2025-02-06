23c80aa1b42ecd4e233be3c3ecd29dfa6a0eb0d7
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-02-06 22:36:15 +0700

    Adding some items in Profile

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -7,7 +7,7 @@ class ProfileScreen extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(80),
         child: Container(
-            height: 120,
+            height: 140,
             decoration: BoxDecoration(
               color: Colors.blue,
               borderRadius: BorderRadius.only(
@@ -26,14 +26,47 @@ class ProfileScreen extends StatelessWidget {
                 child: Row(
               children: [
                 IconButton(
-                  icon: Icon(Icons.person,
-                      color: Colors.white, size: 30), // Ukuran ikon
+                  padding: EdgeInsets.only(
+                      left: 16.0), // Adds space on the left side
+                  icon: Container(
+                    padding: EdgeInsets.all(
+                        12), // This adds space around the icon, making the background circular
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Circular background color
+                      shape: BoxShape.circle, // Makes the background circular
+                    ),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.blue,
+                      size: 30,
+                    ),
+                  ),
                   onPressed: () {
-                    // Tambahkan aksi yang diinginkan, misalnya:
-                    Navigator.pushNamed(context, '/profile');
+                    // Your onPressed logic here
                   },
-                  tooltip:
-                      'Profile', // Menampilkan tooltip saat ikon ditekan lama
+                ),
+                Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Align everything to the left
+                  children: <Widget>[
+                    // First row for Name
+                    Text(
+                      'Name', // Display the name
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+
+                    // Second row for Followers and Following
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .start, // Align them to the start of the row
+                      children: <Widget>[
+                        Text('Followers: 0', style: TextStyle(fontSize: 16)),
+                        SizedBox(width: 20), // Adds space between the two texts
+                        Text('Following: 0', style: TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                  ],
                 )
               ],
             ))),

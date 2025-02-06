e14ea6689f72f0ae1e29260a046dc0e785e199e1
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-02-06 22:09:44 +0700

    Adding some items in Profile

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -5,12 +5,39 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80), 
-        child: SafeArea(
-          child: Row(
-            
-          )
-          )),
+        preferredSize: Size.fromHeight(80),
+        child: Container(
+            height: 120,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: SafeArea(
+                child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.person,
+                      color: Colors.white, size: 30), // Ukuran ikon
+                  onPressed: () {
+                    // Tambahkan aksi yang diinginkan, misalnya:
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                  tooltip:
+                      'Profile', // Menampilkan tooltip saat ikon ditekan lama
+                )
+              ],
+            ))),
+      ),
       body: Center(child: Text('Ini halaman Profile')),
     );
   }

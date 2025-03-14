00b0014753047337719f1260d41874996cf27566
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-03-14 16:08:37 +0700

    Adding firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Make sure this file exists
 import 'views/onboarding/welcome_screen.dart';
 import 'views/onboarding/agreement_screen.dart';
 import 'views/onboarding/login_screen.dart';
 import 'views/onboarding/signup_screen.dart';
 import 'views/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -27,5 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
